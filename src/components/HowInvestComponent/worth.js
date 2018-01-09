7c2db09c77a24e7f752ffc6e4df591329e27515c
Quyunfeng <510689947@qq.com>
2018-01-09 19:29:22 +0800

    '页面样式完成'

---
+++ src/components/HowInvestComponent/worth.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default class Worth extends React.Component {
+
+  render() {
+    return (
+      <div className="section sec-worth autosize">
+        <img className="big" src={require('../../assets/img/invest/pic3.png')} />
+        <div className="w"/>
+      </div>
+    );
+  }
+}
