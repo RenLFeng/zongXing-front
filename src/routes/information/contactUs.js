97acea110e8ebbd72e20cbe388681dce31f2bff8
yyrobot <121415576@qq.com>
2018-02-06 12:03:01 +0800

    增加信息披露联系我们模块 /information/contactUs.js

---
+++ src/routes/information/contactUs.js
@@ -0,0 +1,22 @@
+import React from 'react';
+export default class ContactUs extends React.Component {
+  render() {
+    return (
+      <div className="fr shadow">
+        <p className="c6">联系方式</p>
+        {/*<p className="q">1.备案登记信息<i className="dl"/></p>*/}
+        <p className="a">
+          咨询电话：	400-***-****<br/>
+          投诉、举报电话：	(010) ****-****<br/>
+          传真：	(010) ****-****<br/>
+          服务时间：	9:00 - 21:00<br/>
+          咨询邮箱：	zjb***@****.com<br/>
+          投诉、举报邮箱：	zjb***@***.com<br/>
+          在线客服：	联系在线客服<br/>
+          通讯地址：	北京市朝阳区*****<br/>
+        </p>
+
+      </div>
+    );
+  }
+}
