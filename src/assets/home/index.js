84f828a9859f749902b18ce2f60e4395def25848
Quyunfeng <510689947@qq.com>
2018-02-23 11:17:48 +0800

    提交代码

--- src/assets/home/index.js
+++ src/assets/home/index.js
@@ -1,20 +1,68 @@
 import './index.scss';
 import {animate} from './animate';
+import {init} from './_invest';
 
 export function startAnimate() {
+  initPage();
+  bindEvent();
   animate();
 }
 
+function initPage() {
 
-export function initPage() {
-    $('.banner .swiper-slide').css('width', $(window).width());
-    var swiper = new Swiper('.swiper-container', {
-        autoplay: 5000,
-        speed: 1000,
-        loop: true,
-        pagination: '.swiper-pagination',
-        paginationClickable: true
-    });
+  new TextSlider($('.home-banner .t1'));
+
 }
 
+function bindEvent() {
+  $('.home-banner').on('click', function(){
+    $('.home-banner video')[0].play();
+  });
+}
 
+class TextSlider {
+  constructor(arr){
+    this.arr = arr;
+    this.init();
+  }
+  init(){
+    let _this = this;
+
+    let i0 = this.arr.eq(0).find('i');
+    let i1 = _this.arr.eq(1).find('i');
+    let i2 = _this.arr.eq(2).find('i');
+
+    _this.first(i0);
+    setTimeout(function(){
+      i1.addClass('show');
+      setTimeout(function(){
+        i1.removeClass('show');
+      },4000);
+    }, 12000);
+    setTimeout(function(){
+      i2.addClass('show');
+      setTimeout(function(){
+        i2.removeClass('show');
+      },4000);
+      setTimeout(function(){
+        _this.init();
+      },7000);
+    }, 19000);
+  }
+  first(ii){
+    ii.eq(0).addClass('show');
+    setTimeout(function(){
+      ii.eq(1).addClass('show');
+    },2000);
+    setTimeout(function(){
+      ii.eq(2).addClass('show');
+    },4000);
+    setTimeout(function(){
+      ii.parent().addClass('hid');
+      ii.removeClass('show');
+    }, 8000);
+    setTimeout(function(){
+      ii.parent().removeClass('hid');
+    },10000);
+  }
+}
--- src/components/HomePageComponent/banner2.js
+++ src/components/HomePageComponent/banner2.js
@@ -9,43 +9,57 @@ import Path from '../../common/pagePath';
   systemData: state.systemData
 }))
 export default class Banner extends React.Component {
+  state = {
+    show: false
+  };
 
   componentDidMount() {
-    this.fetchHomeImg();
+    // this.fetchHomeImg();
+    setTimeout(()=>{
+      this.setState({
+        show: true
+      });
+    }, 100)
   }
   fetchHomeImg() {
-    const {systemData} = this.props;
-    //如果图片是个数是0 则重新加载图片资源
-    if (systemData.homeImg.length === 0) {
-      this.props.dispatch({
-        type: 'systemData/getHomeImg',
-        payload: 'home'
-      });
-    } else {
-      initPage();
-    }
+    // const {systemData} = this.props;
+    // //如果图片是个数是0 则重新加载图片资源
+    // if (systemData.homeImg.length === 0) {
+    //   this.props.dispatch({
+    //     type: 'systemData/getHomeImg',
+    //     payload: 'home'
+    //   });
+    // } else {
+    //   initPage();
+    // }
   }
   render() {
     const {systemData} = this.props;
     return (
       <div className="section home-banner">
-        {/*<video autoplay="autoplay" loop="loop">*/}
-          {/*<source src="https://n1video.hjfile.cn/zhuanti/2018/02/05/5b543a2fc89d1f464e152dc4015eea50.webm" type="video/webm">*/}
-            {/*<source src="https://n1video.hjfile.cn/zhuanti/2018/02/05/5f381bab717a30c3c91f9dfae6d8334e.mp4" type="video/mp4">*/}
-        {/*</video>*/}
-        {/*<Video */}
-               {/*source={require('https://n1video.hjfile.cn/zhuanti/2018/02/05/5b543a2fc89d1f464e152dc4015eea50.webm')}*/}
-               {/*style={styles.fullScreen}*/}
+        <video autoPlay="autoplay" loop="loop" preload="metadata" controls="controls">
+          <source src="https://n1video.hjfile.cn/zhuanti/2018/02/05/5b543a2fc89d1f464e152dc4015eea50.webm" type="video/webm"/>
+          <source src="https://n1video.hjfile.cn/zhuanti/2018/02/05/5f381bab717a30c3c91f9dfae6d8334e.mp4" type="video/mp4"/>
+        </video>
+        {/*<video*/}
+          {/*preload="metadata" controls="controls"*/}
+          {/*src='https://n1video.hjfile.cn/zhuanti/2018/02/05/5b543a2fc89d1f464e152dc4015eea50.mp4'*/}
         {/*/>*/}
+        {/*<video className="hide"*/}
+               {/*preload="metadata" controls="controls"*/}
+               {/*poster={require('../../assets/img/home/1.jpg')}*/}
+               {/*src="//n1video.hjfile.cn/zhuanti/2017/12/20/f5339abc403997a43fa9b9362b2a29cb.mp4">*/}
+          {/*您的浏览器不支持视频播放，请升级浏览器*/}
+        {/*</video>*/}
         <div className="masker"></div>
         <div className="w">
-          <p className="t1">
+          <p className="t1" style={{display: `${this.state.show?'block':'none'}`}}>
             <i className="h">小微企业</i><i>能<span className="h">精准营销</span></i><i>的<span className="h">借贷平台</span></i>
           </p>
-          <p className="t1 hid">
+          <p className="t1 hid" style={{display: `${this.state.show?'block':'none'}`}}>
             <i>个人可以直接投资中国实体经济</i>
           </p>
-          <p className="t1 hid">
+          <p className="t1 hid" style={{display: `${this.state.show?'block':'none'}`}}>
             <i>通过帮助小微企业成功，获得丰厚回报</i>
           </p>
           <p className="t2"><a className="btn">我要投资</a></p>
