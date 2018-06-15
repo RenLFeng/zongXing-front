b347626c98a95cac1e689b5f46e8010d41c6d714
yangk <724144845@qq.com>
2018-06-15 14:45:22 +0800

    资金动态

--- src/components/Account/accountstatement.js
+++ src/components/Account/accountstatement.js
@@ -6,6 +6,7 @@ import {pageShows} from "../../common/systemParam";
 import LeftMenu from '../UCenterComponent/leftMenu';
 import {accountService} from '../../services/api2.js';
 import { Steps } from 'antd';
+import Statement from '../common/Statement';
 import '../../assets/account/accountstatement.scss';
 /**
  * 资金流水界面
@@ -20,11 +21,20 @@ export default class AccountStatement extends React.Component{
       infoList:[],   //资金动态列表
       num:'',
       maxPage: 0,     //最大页
+      //业务类型码:1201:充值，1301：提现，1405：项目还款，1404：项目投资，若为null,则全查
+      lables:[
+        {lable:'总账',code:'0000'},
+        {lable:'充值',code:'1201'},
+        {lable:'提现',code:'1301'},
+        {lable:'投资',code:'1404'},
+        {lable:'回款',code:'1405'},
+      ],
+      activeCode:'0000',
     }
   }
 
   componentDidMount() {
-    //this.getCapitalDynamics(1);  //调用请求
+    this.getCapitalDynamics(1);  //调用请求
   }
 
   async getCapitalDynamics(page){
@@ -36,16 +46,16 @@ export default class AccountStatement extends React.Component{
         pageSize: this.state.pageSize
       };
       const res = await accountService.getAccountStatement(param);
-      console.log(res);
+   
       if(res.code === 0){ 
+        console.log(res);
         //const maxPage = Math.ceil(res.data.totalNumber / this.state.pageSize);
         // this.setState({
         //   maxPage:maxPage,
         //   infoList:res.data.infoList,
         //   pageCurrent: page,
         //   num:res.data.totalNumber
-        // });
-
+        // }); 
       } else {
         res.msg && message.error(res.msg);
       }
@@ -58,6 +68,14 @@ export default class AccountStatement extends React.Component{
     }
   }
 
+  //方法区域
+  // 点击选择类型
+  handlerClcikLable=(code)=>{
+     this.setState({
+      activeCode:code,
+     });
+  }
+
   render(){ 
     const Step = Steps.Step;
     return(
@@ -68,21 +86,37 @@ export default class AccountStatement extends React.Component{
             <div className='search-area'>
                 <p className='top-title'>资金流水 </p>
                 <ul className='search-tag'>
-                    <li className='active'>全部信息</li>
-                    <li>充值</li>
-                    <li>提现</li>
-                    <li>投资</li>
-                    <li>回款</li>  
+                    {
+                      this.state.lables.map(item=>{
+                        return <li onClick={this.handlerClcikLable.bind(this,item.code)} className={item.code===this.state.activeCode?'active':''} >{item.lable}</li>
+                      })
+                    } 
                 </ul>  
             </div>  
             {/* 内容区域 */}
-          <div >
-            <Steps current={1} progressDot direction="vertical" size="small">
-                <Step title="Finished" description="You can hover on the dot." />
-                <Step title="In Progress" description="You can hover on the dot." />
-                <Step title="Waiting" description="You can hover on the dot." />
-                <Step title="Waiting" description="You can hover on the dot." />
-            </Steps>
+          <div className='as-list'>
+            {/* 总账 */}
+            <div className={this.state.activeCode==='0000'?'':'hide'}>
+              <Statement showTitle={true}></Statement>
+              <Statement></Statement>
+              <Statement></Statement>
+            </div>
+            {/* 充值 */}
+            <div className={this.state.activeCode==='1201'?'':'hide'}>
+               
+            </div>
+            {/* 提现 */}
+            <div className={this.state.activeCode==='1301'?'':'hide'}>
+           
+            </div>
+            {/* 投资 */}
+            <div className={this.state.activeCode==='1404'?'':'hide'}>
+          
+            </div>
+            {/* 回款 */}
+            <div className={this.state.activeCode==='1405'?'':'hide'}>
+                    
+            </div>
           </div> 
         </div>
       </div> 
