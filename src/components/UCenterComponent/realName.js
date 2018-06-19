ecc68d70e06ebb243fd39eea72666d4fa9dcc13a
fj <1208079349@qq.com>
2018-06-19 12:28:49 +0800

    提交

--- src/components/UCenterComponent/realName.js
+++ src/components/UCenterComponent/realName.js
@@ -483,9 +483,9 @@ export default class RealName extends React.Component {
                   <Steps progressDot current={this._judgeAccount(safeData)} direction="vertical">
                     <Step title="第一步"
                       description={
-                        <div style={{ marginBottom: 65 }}>
+                        <div style={{ marginBottom: 28 }}>
                           <div className="first">
-                            <span style={{ color: '#ff9900', fontSize: '28px', lineHeight: '28px', position: 'absolute', left: '30px', top: '45px' }}>*</span> <span className="left">身份认证</span>
+                            <span style={{ color: 'red', fontSize: '28px', lineHeight: '28px', position: 'absolute', left: '30px', top: '39px' }}>*</span> <span className="left">身份认证</span>
                             <span className="middle">用于提升账户安全性，认证后不能修改</span>
                             {!safeData.userSecurityCenter.fCertification ? <a className="right" onClick={() => this.props.history.push(AUTHENTICATION)}>立即认证</a> : null}
                           </div>
@@ -501,18 +501,18 @@ export default class RealName extends React.Component {
                     <Step
                       title="第二步"
                       description={
-                        <div style={{ marginBottom: 65 }}>
+                        <div style={{ marginBottom:28 }}>
                           <div className="first">
-                            <span style={{ color: '#ff9900', fontSize: '28px', lineHeight: '28px', position: 'absolute', left: '30px', top: '45px' }}>*</span> <span className="left">开通乾多多资金托管账户</span>
+                            <span style={{ color: 'red', fontSize: '28px', lineHeight: '28px', position: 'absolute', left: '30px', top: '39px' }}>*</span> <span className="left">开通乾多多资金托管账户</span>
                             <span className="middle">开通资金托管账户，将投资人、借款人、平台三者的资金完全隔离</span>
                             {!safeData.userSecurityCenter.fThirdAccount ? <a className="right" onClick={() => this.props.history.push(OPENQACCOUNT)}>开通账户</a> : null}
                           </div>
-                          <div style={{ marginTop: 30, marginBottom: 8 }}>
+                          <div >
                             <img alt="" src={require('../../assets/img/ucenter/u4288.png')} />
                           </div>
                           {
                             safeData.userSecurityCenter.fThirdAccount ?
-                              <div className="personal">
+                              <div className="personal" style={{marginTop:0}}>
                                 <span style={{ color: 'black' }} >你的钱多多账户:{safeData.fThirdAccountNo}</span>
                                 <div className="findPass">
                                   <span className="a" onClick={() => this.setState({ showMMMChangepayPassword: true, showMMMChangeLoginPass: false })}>找回乾多多支付密码 </span>
@@ -534,9 +534,9 @@ export default class RealName extends React.Component {
                       } />
                     <Step
                       title="第三步" description={
-                        <div style={{ marginBottom: 65 }}>
+                        <div style={{ marginBottom: 28 }}>
                           <div className="first">
-                            <span style={{ color: '#ff9900', fontSize: '28px', lineHeight: '28px', position: 'absolute', left: '30px', top: '45px' }}>*</span> <span className="left">我的银行卡</span>
+                            <span style={{ color: 'red', fontSize: '28px', lineHeight: '28px', position: 'absolute', left: '30px', top: '39px' }}>*</span> <span className="left">我的银行卡</span>
                             <span className="middle">至少绑定一张本人开户的银行卡，最多可绑定5个银行卡</span>
                             <a className="right" style={{ display: 'none' }}>设置</a>
                           </div>
@@ -591,19 +591,6 @@ export default class RealName extends React.Component {
                     />
                   </Steps>
                 </div>
-                {/* {
-                  safeData.userSecurityCenter.fCertification !== undefined ?
-                      <div className="fr uc-rbody ant-steps-item-description" style={{marginTop:'30px'}}>
-                        <div className="user_basic fl" onClick={() => this.props.history.push(USER_BASIC)}>
-                          <p className="ic"><b></b></p>
-                          <p className="tit">基础资料</p>
-                          <p className="info in1">完善个人信息，增强账户安全等级</p>
-                          <p className="info in2">
-                            <i>*</i><i>*</i><i>*</i><i>*</i><i>*</i>
-                          </p>
-                        </div>
-                      </div> :null
-                } */}
               </div> : null}
 
               {safeData.userSecurityCenter.fCertification ?
